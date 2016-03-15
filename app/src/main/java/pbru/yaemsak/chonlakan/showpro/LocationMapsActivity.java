04b478697295c878e93930e8effafbb2025508de
masterUNG <phrombutr@gmail.com>
2016-03-15 17:39:38 +0700

    Get Location Finish

--- app/src/main/java/pbru/yaemsak/chonlakan/showpro/LocationMapsActivity.java
+++ app/src/main/java/pbru/yaemsak/chonlakan/showpro/LocationMapsActivity.java
@@ -1,7 +1,13 @@
 package pbru.yaemsak.chonlakan.showpro;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +19,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class LocationMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private boolean GPSABoolean, networkABoolean;
+    private double myLatADouble, myLngADouble;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +32,125 @@ public class LocationMapsActivity extends FragmentActivity implements OnMapReady
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //Open Service Location
+        openServiceLocation();
+
+
+    }   // Main Method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        locationManager.removeUpdates(locationListener);
+        myLatADouble = 0;
+        myLngADouble = 0;
+
+        Location networkLocation = requestLocation(LocationManager.NETWORK_PROVIDER, "Cannot Connected Internet");
+        if (networkLocation != null) {
+            myLatADouble = networkLocation.getLatitude();
+            myLngADouble = networkLocation.getLongitude();
+        }
+
+        Location GPSLocation = requestLocation(LocationManager.GPS_PROVIDER, "NO card GPS");
+        if (GPSLocation != null) {
+            myLatADouble = GPSLocation.getLatitude();
+            myLngADouble = GPSLocation.getLongitude();
+        }
+
+        Log.d("15March", "myLat ==> " + myLatADouble);
+        Log.d("15March", "myLng ==> " + myLngADouble);
+
+    }   // onResume
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        GPSABoolean = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!GPSABoolean) {
+
+            networkABoolean = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!networkABoolean) {
+
+                Log.d("15March", "Cannot Find My Location");
+
+            }   // if1
+
+        }   //if1
+
+
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        locationManager.removeUpdates(locationListener);
+
+    }
+
+    public Location requestLocation(String strProvider, String strError) {
+
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+
+            locationManager.requestLocationUpdates(strProvider,
+                    1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+
+        } else {
+
+            Log.d("15March", "Error ==> " + strError);
+
+        }   // if
+
+        return location;
+    }   // requestLocation
+
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            myLatADouble = location.getLatitude();
+            myLngADouble = location.getLongitude();
+
+        }   // onLocationChange
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+
+
+    private void openServiceLocation() {
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+    }   // openServiceLocation
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -42,5 +159,6 @@ public class LocationMapsActivity extends FragmentActivity implements OnMapReady
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+    }   // onMapReady
+
+}   // Main Method
